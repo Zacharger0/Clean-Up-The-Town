ad27d429a478884c34ec51a5964ec1d0a9f56634
Zach Harger <zacharger0@gmail.com>
2025-10-21 12:42:20 -0400

    replace magnet with vacuum function
    now, the player needs to hold down the space bar and be in range of the trash to pick it up.

--- trash/cardboard_box.gd
+++ trash/cardboard_box.gd
@@ -2,29 +2,32 @@ extends Area2D
 
 signal trash_collected
 
-var time = 1
-var freq = 10
-var amplitude = 1
-
-var start_y: float
-var phase_offset 
-
+var move_speed = 50.0  # SPEED THAT TRASH MOVES TO PLAYER
+var activation_distance = 100.0  # MAX DISTANCE FOR MAGNET EFFECT
 var player: CharacterBody2D
 
 func _ready() -> void:
+	# CONNECTS body_entered SIGNAL
 	connect("body_entered", Callable(self, "_on_body_entered"))
-#	start_y = position.y 
-#	phase_offset = randf() * TAU
-	
-func _on_body_entered(body: Node) -> void:
-	if body is CharacterBody2D:  
-		trash_collected.emit()
-		queue_free()
 
-#func _physics_process(delta: float) -> void:
-#	wobble(delta)
-	
-#func wobble(delta):
-#	position.y = start_y + sin(time * freq + phase_offset) * amplitude
-#	time += delta
-	
+	# FINDS PLAYER NODE GROUP
+	player = get_tree().get_first_node_in_group("player")
+	if not player:
+		push_warning("No player found in group 'player'. Box won't move.")
+
+func _physics_process(delta: float) -> void:
+	if player:
+		# CALCULATE DISTANCE TO PLAYER
+		var distance = global_position.distance_to(player.global_position)
+		# CHECKS IF SPACEBAR IS HELD DOWN /AND/ TRASH IS IN RANGE
+		if Input.is_action_pressed("ui_accept") and distance < activation_distance:
+			# CALCULATES DIRECTION TOWARD PLAYER
+			var direction = (player.global_position - global_position).normalized()
+			# MOVES TO PLAYER
+			position += direction * move_speed * delta
+
+func _on_body_entered(body: Node) -> void:
+	if body is CharacterBody2D:
+		trash_collected.emit()
+		
+		queue_free()
